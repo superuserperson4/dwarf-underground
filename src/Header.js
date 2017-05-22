81fbd25e32e19aeeb67eb75261b319332e905aa9
Jack Goldstein <superuserperson4@gmail.com>
2017-05-22 17:48:05 -0400

    Make Header Components

---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './Header.css'
+import NavLinks from './NavLinks.js'
+import Logo from './Logo.js'
+
+class Header extends Component {
+    render() {
+        return(
+            <div className="expanded row header">
+                <div className="clearfix">
+                    <Logo/>
+                    <NavLinks/>
+                </div>
+                <div className="headline text-center small-12 columns">
+                    <h1>The Dwarf Underground</h1>
+                    <h3>Delving deep for the <span className="gold">shiniest</span> news</h3>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Header
